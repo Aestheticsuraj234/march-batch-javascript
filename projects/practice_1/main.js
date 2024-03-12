a078956d7caa108ab388be679b02fc1c120e90a8
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-12 20:58:15 +0530

    Refactor code for improved performance and readability

---
+++ projects/practice_1/main.js
@@ -0,0 +1,30 @@
+const RANDOM_NUMBER =  Math.floor(Math.random() * 100) + 1;
+
+console.log(RANDOM_NUMBER);
+
+const guessForm = document.getElementById("guessForm");
+const Message = document.getElementById("message");
+
+guessForm.addEventListener("submit",function(e){
+
+    e.preventDefault();
+
+    let userGuess = parseInt(document.getElementById("guess").value);
+
+
+
+    if(userGuess === RANDOM_NUMBER){
+        Message.innerHTML = "Congratulations! You guessed the right number"
+        Message.classList.add("text-green-500")
+       
+    }
+    else{
+        
+        Message.innerHTML = `Sorry, the number was ${RANDOM_NUMBER}. Try again!`;
+        Message.classList.add("text-red-500")
+
+    }
+
+    guessForm.reset();
+
+})
